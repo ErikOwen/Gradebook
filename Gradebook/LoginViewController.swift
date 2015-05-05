71b8190b19874b8b617717f0ae630f0c82c8cbc7
Erik Owen <erikjowen@yahoo.com>
2015-05-05 10:13:10 -0700

    added connection between storyboard and controller

--- Gradebook/LoginViewController.swift
+++ Gradebook/LoginViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        println("button pressed");
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
